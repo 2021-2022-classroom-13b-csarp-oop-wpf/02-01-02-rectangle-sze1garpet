41573c140a2ef52821ee8008f094b89ccc12dd5c
Garzó Péter <sze1garpet@vasvari.org>
2022-04-06 15:59:44 +0200

    konstruktor és mezők

---
+++ RectangleProject/RectangleProject/Models/Rectangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RectangleProject.Model
+{
+    class Rectangle
+    {
+        // mezőnevek
+        private double sideA;
+        private double sideB;
+
+        // konstruktor példányosításakor használjuk
+        public Rectangle(double sideA, double sideB)
+        {
+            this.sideA = sideA;
+            this.sideB = sideB;
+        }
+    }
+}
